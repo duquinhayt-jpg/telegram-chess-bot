8514f5b0f96988ef69e6cbacd70760ac41f1248c
duquinhayt-jpg <duquinhayt@gmail.com>
2026-03-11 00:56:35 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,4 @@
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from stockfish import Stockfish
 
-TOKEN = "8625933223:AAH4SppnDG5LqIfn-k3bN35KOOB_JoKRWGc"
-
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Bot online!")
-
-app = ApplicationBuilder().token(TOKEN).build()
-app.add_handler(CommandHandler("start", start))
-
-app.run_polling()
+stockfish = Stockfish(path="/usr/games/stockfish")
 
